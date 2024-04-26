e1fdf64c6593f89e01b414abde180aa78586aace
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2024-04-26 12:20:52 -0600

    CNew file to calculate area of a circle

---
+++ testfile.py
@@ -0,0 +1,10 @@
+# Python
+
+import math
+
+def calculate_cirlce_area(radius):
+    area = math.pi * radius * radius
+    return area
+
+area = calculate_cirlce_area(10)
+print(f'The area of the circle is: {area}')
