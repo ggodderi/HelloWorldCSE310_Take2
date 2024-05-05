fe5bde539b427f8b44f6e8630669e5d41a38ccfd
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2024-05-05 13:08:34 -0600

    Added calculate Rectangle

--- shapes.py
+++ shapes.py
@@ -8,9 +8,12 @@ def calculate_circle_area(radius):
     area = math.pi * radius * radius
     return area
 
+def calculate_rectangle_area(width, length):
+    area = width * length
+    return area
 
 def main():
     print(f'Circle Area is: {calculate_circle_area(10)}')
-
+    print(f'Circle Area is: {calculate_rectangle_area(10, 30)}')
 
 main()
